2e15d5c2c1f774fcbfb581c7b29372d4eb2b24d3
Yehwala <obssiyehwala@gmail.com>
2026-07-02 15:27:35 +0300

    fix: add CLIENT_URL to CORS allowed origins

--- server/server.js
+++ server/server.js
@@ -43,8 +43,9 @@ app.use(cors({
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
-    ],
+        'http://127.0.0.1:5173',
+        process.env.CLIENT_URL
+    ].filter(Boolean),
     credentials: true
 }));
 
